155fbfb298e995ffdaac90670c740f906eaef9b4
Andrii Vospitannikov <andrii.vospitannikov@sap.com>
2021-04-27 18:16:40 +0300

    log execution time (#221)

--- OwnID.Server/Metrics/MetricsConfiguration.cs
+++ OwnID.Server/Metrics/MetricsConfiguration.cs
@@ -1,3 +1,6 @@
+using System;
+using OwnID.Extensibility.Configuration.Validators;
+
 namespace OwnID.Server.Metrics
 {
     public class MetricsConfiguration
@@ -9,5 +12,24 @@ namespace OwnID.Server.Metrics
         public uint Interval { get; set; }
         
         public int EventsThreshold { get; set; }
+    }
+
+    public class MetricsConfigurationValidator : IConfigurationValidator<MetricsConfiguration>
+    {
+        public void FillEmptyWithOptional(MetricsConfiguration configuration)
+        {
+            if (configuration.Interval == default)
+                configuration.Interval = 60000;
+
+            if (configuration.EventsThreshold == default)
+                configuration.EventsThreshold = 100;
+        }
+
+        public void Validate(MetricsConfiguration configuration)
+        {
+            if (string.IsNullOrWhiteSpace(configuration.Namespace))
+                throw new InvalidOperationException(
+                    $"{nameof(configuration.Namespace)} for metrics config is required");
+        }
     }
 }
